89edeacf2d284b23cd93a2e0603fbd6d200159eb
Angela-Haim <hei.angela.5@gmail.com>
2026-09-18 13:37:18 +0300

    Declaration de la fonction NegatifPositif() avec une boucle et les conditions

---
+++ Exercice1.js
@@ -0,0 +1,14 @@
+
+
+const NegatifPositif =  (tableau) => {
+    let resultat = [];
+    for (let i = 0; i < tableau.length; i++) {
+        if (tableau[i] <= 0) {
+           
+        } else if (tableau[i] > 0) {
+            
+        }
+        
+    }
+console.log(NegatifPositif([3, -1, 0, 4, -2, 5, -3]));
+} 
